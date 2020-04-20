112d29b6d1797a1ea8fea86d7250962d5c4a90ce
nicolastm <nicolasterramaia@gmail.com>
2020-04-19 22:41:41 -0300

    PouchDBFind plugin added

--- js/constantes/pouchdbPlugins.js
+++ js/constantes/pouchdbPlugins.js
@@ -1,6 +1,9 @@
-import PouchDB from "pouchdb-react-native";
-import PouchDBAuthentication from "pouchdb-authentication";
-import HttpPouch from "pouchdb-adapter-http";
+import PouchDB from 'pouchdb-react-native';
+import PouchDBAuthentication from 'pouchdb-authentication';
+import HttpPouch from 'pouchdb-adapter-http';
+import PouchDBFind from 'pouchdb-find';
 
-PouchDB.debug.enable("*");
-export default PouchDB.plugin(PouchDBAuthentication).plugin(HttpPouch);
+PouchDB.debug.enable('*');
+export default PouchDB.plugin(PouchDBAuthentication)
+	.plugin(HttpPouch)
+	.plugin(PouchDBFind);
