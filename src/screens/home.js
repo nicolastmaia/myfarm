0bb88930d2712f3dd0580dade1af8100bf71d3b8
nicolastm <nicolasterramaia@gmail.com>
2020-07-24 19:59:17 -0300

    refactor: icons changed and objects' positions corrected

--- src/screens/home.js
+++ src/screens/home.js
@@ -5,8 +5,8 @@ import {
 	Dimensions,
 	ImageBackground,
 } from 'react-native';
-import { View, Text, Button, Container } from 'native-base';
-import Ionicon from 'react-native-vector-icons/Ionicons';
+import { View, Text, Container } from 'native-base';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { imagemClima } from '../componentes/previsaoTempo';
 
 export default class Home extends React.Component {
@@ -26,20 +26,14 @@ export default class Home extends React.Component {
 				this.setState({
 					tempo: {
 						descricao: tmp[0].toUpperCase() + tmp.slice(1),
-						temperatura: (resJson.main.temp - 273).toFixed(0) + ' º',
-						icone: imagemClima(resJson.weather[0].icon, 60, 60),
+						temperatura: (resJson.main.temp - 273).toFixed(0) + 'º',
+						icone: imagemClima(resJson.weather[0].icon, 80, 80),
 						cidade: resJson.name,
 					},
 				});
 			})
 			.catch((erro) => Alert.alert(erro.message));
 	}
-	//configs enviadas ao navegadores.js
-	static navigationOptions = {
-		tabBarIcon: ({ tintColor }) => (
-			<Ionicon name="md-home" color={tintColor} style={{ fontSize: 30 }} />
-		),
-	};
 
 	render() {
 		const { navigate, openDrawer } = this.props.navigation;
@@ -48,7 +42,7 @@ export default class Home extends React.Component {
 		//config do icone de previsao do tempo
 		if (this.state.tempo.descricao == '') {
 			tmp = (
-				<Text style={{ color: '#FFFFFF', fontSize: 20 }}>
+				<Text style={{ color: '#FFFFFF', fontSize: 40 }}>
 					Carregando...
 				</Text>
 			);
@@ -57,6 +51,9 @@ export default class Home extends React.Component {
 				<>
 					<View
 						style={{
+							borderStyle: 'solid',
+							borderRightWidth: 1,
+							width: Dimensions.get('window').width / 2,
 							alignItems: 'center',
 							justifyContent: 'center',
 						}}
@@ -65,6 +62,7 @@ export default class Home extends React.Component {
 						<Text
 							style={{
 								color: '#fff',
+								fontSize: 30,
 							}}
 						>
 							{this.state.tempo.cidade}
@@ -74,19 +72,16 @@ export default class Home extends React.Component {
 					<View
 						style={{
 							borderStyle: 'solid',
-							borderLeftWidth: 2,
+							borderLeftWidth: 1,
+							width: Dimensions.get('window').width / 2,
 							alignItems: 'center',
 							justifyContent: 'center',
-							paddingLeft: 15,
-							paddingBottom: 5,
-							paddingTop: 15,
-							marginLeft: 15,
 						}}
 					>
 						<Text
 							style={{
 								color: '#fff',
-								fontSize: 40,
+								fontSize: 60,
 							}}
 						>
 							{this.state.tempo.temperatura}
@@ -95,6 +90,7 @@ export default class Home extends React.Component {
 						<Text
 							style={{
 								color: '#fff',
+								fontSize: 30,
 							}}
 						>
 							{this.state.tempo.descricao}
@@ -116,29 +112,31 @@ export default class Home extends React.Component {
 					}}
 				>
 					{/* botao menu sanduiche */}
-					<TouchableOpacity
-						style={{
-							position: 'absolute',
-							marginTop: 35,
-							marginLeft: 15,
-						}}
-						onPress={() => openDrawer()}
-					>
-						<Ionicon
-							style={{ color: 'white', fontSize: 45 }}
-							name="md-menu"
-						/>
-					</TouchableOpacity>
+					<View style={{ flex: 1 }}>
+						<TouchableOpacity
+							style={{
+								position: 'absolute',
+								marginTop: 35,
+								marginLeft: 15,
+							}}
+							onPress={() => openDrawer()}
+						>
+							<Icon
+								style={{ color: 'white', fontSize: 45 }}
+								name="menu"
+							/>
+						</TouchableOpacity>
+					</View>
 
-					<View style={{ marginTop: 35 }}>
+					<View style={{ flex: 5 }}>
 						{/* previsao do tempo */}
 						<View
 							style={{
-								marginTop: 50,
+								flex: 1,
 								flexDirection: 'row',
 								height: 95,
 								alignItems: 'center',
-								justifyContent: 'center',
+								justifyContent: 'space-evenly',
 							}}
 						>
 							{tmp}
@@ -147,7 +145,7 @@ export default class Home extends React.Component {
 						{/* botoes principais */}
 						<View
 							style={{
-								marginTop: 30,
+								flex: 4,
 								justifyContent: 'center',
 								alignItems: 'center',
 								flexDirection: 'column',
@@ -161,8 +159,8 @@ export default class Home extends React.Component {
 									style={styles.botoes}
 									onPress={() => navigate('Aplicacao')}
 								>
-									<Ionicon
-										name="md-trending-up"
+									<Icon
+										name="flask-plus-outline"
 										style={styles.icone}
 									/>
 									<Text style={styles.texto}>Aplicações</Text>
@@ -174,8 +172,8 @@ export default class Home extends React.Component {
 									style={styles.botoes}
 									onPress={() => navigate('Perdas')}
 								>
-									<Ionicon
-										name="md-trending-down"
+									<Icon
+										name="numeric-negative-1"
 										style={styles.icone}
 									/>
 									<Text style={styles.texto}>Perdas</Text>
@@ -190,7 +188,7 @@ export default class Home extends React.Component {
 									style={styles.botoes}
 									onPress={() => navigate('Talhao')}
 								>
-									<Ionicon name="md-grid" style={styles.icone} />
+									<Icon name="grid-large" style={styles.icone} />
 									<Text style={styles.texto}>Talhão</Text>
 								</TouchableOpacity>
 
@@ -200,7 +198,7 @@ export default class Home extends React.Component {
 									style={styles.botoes}
 									onPress={() => navigate('Colheita')}
 								>
-									<Ionicon name="md-basket" style={styles.icone} />
+									<Icon name="basket-outline" style={styles.icone} />
 									<Text style={styles.texto}>Colheita</Text>
 								</TouchableOpacity>
 							</View>
@@ -219,8 +217,8 @@ const styles = StyleSheet.create({
 		flexDirection: 'column',
 		justifyContent: 'center',
 		alignItems: 'center',
-		height: 150,
-		width: 150,
+		height: 180,
+		width: 180,
 		margin: 10,
 		borderRadius: 10,
 	},
