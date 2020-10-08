db4e6df06dfd58eeaed067453ba74dd706365899
Nicolas Terra Maia <nicolasterramaia@gmail.com>
2020-10-08 16:22:11 -0300

    refact: cadColheita component converted from class to functional component

--- src/screens/cadColheita.js
+++ src/screens/cadColheita.js
@@ -1,39 +1,10 @@
-import React from 'react';
-import {
-	Platform,
-	StatusBar,
-	Dimensions,
-	Image,
-	ScrollView,
-	Keyboard,
-	Slider,
-	FlatList,
-	ListView,
-} from 'react-native';
-import {
-	Item,
-	Card,
-	List,
-	ListItem,
-	View,
-	Input,
-	Icon,
-	Button,
-	Text,
-	Toast,
-	Header,
-	Left,
-	Right,
-	Body,
-	Content,
-	Container,
-} from 'native-base';
+import React, { useEffect, useState } from 'react';
+import { Card, Button, Text, Content, Container } from 'native-base';
 
 // const analytics = require("./instancias/analytics");
 import { Banco } from '../instancias/conexao.js';
 
-import { Texto, Formulario } from '../componentes/customizado';
-import CustomHeader from '../componentes/customHeader';
+import { Formulario } from '../componentes/customizado';
 
 var form1 = [
 	{ nome: 'data', placeholder: 'Data da colheita', tipo: 'data' },
@@ -43,82 +14,73 @@ var form1 = [
 	{ nome: 'observacoes', placeholder: 'Observações' },
 ];
 
-export default class CadColheita extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+export default function CadColheita(props) {
+	const [itens, setItens] = useState([]);
+	const [formulario1, setFormulario1] = useState({});
+	const { navigation } = props;
+	const { params } = props.route;
+	const { goBack } = navigation;
 
-	componentDidMount = () => {
+	useEffect(() => {
 		// let data = new Date();
 		// this._formulario1.setValor("data",data.getDate() + "/" + (data.getMonth() + 1) + "/" + data.getFullYear());
-	};
+	});
 
-	render() {
-		const { getParam, goBack } = this.props.navigation;
-		return (
-			<Container>
-				{/* <CustomHeader titulo="Cadastro de Colheitas" /> */}
+	return (
+		<Container>
+			{/* <CustomHeader titulo="Cadastro de Colheitas" /> */}
 
-				<Content style={{ backgroundColor: '#eee', padding: 15 }}>
-					<Text style={{ fontSize: 18, marginLeft: 5 }}>COLHEITA</Text>
-					<Text
-						style={{
-							fontSize: 14,
-							color: '#444',
-							marginBottom: 8,
-							marginLeft: 5,
-						}}
-					>
-						Registro da produção diária.
-					</Text>
+			<Content style={{ backgroundColor: '#eee', padding: 15 }}>
+				<Text style={{ fontSize: 18, marginLeft: 5 }}>COLHEITA</Text>
+				<Text
+					style={{
+						fontSize: 14,
+						color: '#444',
+						marginBottom: 8,
+						marginLeft: 5,
+					}}
+				>
+					Registro da produção diária.
+				</Text>
 
-					<Card style={{ borderRadius: 5, padding: 10 }}>
-						<Formulario
-							tamanho={45}
-							campos={form1}
-							cor="#000"
-							corP="#555"
-							ref={(tmp) => (this._formulario1 = tmp)}
-						/>
-					</Card>
+				<Card style={{ borderRadius: 5, padding: 10 }}>
+					<Formulario tamanho={45} campos={form1} cor='#000' corP='#555' ref={(tmp) => setFormulario1(tmp)} />
+				</Card>
 
-					<Button
-						block
-						style={{
-							marginTop: 15,
-							marginBottom: 25,
-							backgroundColor: '#4c7a34',
-						}}
-						onPress={() => {
-							var tmp = this._formulario1.getValores();
+				<Button
+					block
+					style={{
+						marginTop: 15,
+						marginBottom: 25,
+						backgroundColor: '#4c7a34',
+					}}
+					onPress={() => {
+						var tmp = formulario1.getValores();
 
-							Banco.store('colheita', tmp)
-								.then((response) => {
-									getParam('anterior').setState({
-										itens: response.itens,
-									});
-								})
-								.catch((err) => showDefaultToast(err));
+						Banco.store('colheita', tmp)
+							.then((response) => {
+								params.anterior.setItens(response.itens);
+							})
+							.catch((err) => showDefaultToast(err));
 
-							// let data = new Date();
-							// Banco.registraHistorico("colheitas", {
-							//   time:
-							//     data.getDate() +
-							//     "/" +
-							//     (data.getMonth() + 1) +
-							//     "/" +
-							//     data.getYear(),
-							//   title: tmp.quantidade + " tomates coletados",
-							//   description: tmp.producao
-							// });
+						// let data = new Date();
+						// Banco.registraHistorico("colheitas", {
+						//   time:
+						//     data.getDate() +
+						//     "/" +
+						//     (data.getMonth() + 1) +
+						//     "/" +
+						//     data.getYear(),
+						//   title: tmp.quantidade + " tomates coletados",
+						//   description: tmp.producao
+						// });
 
-							goBack();
-						}}
-					>
-						<Text>Cadastrar</Text>
-					</Button>
-				</Content>
-			</Container>
-		);
-	}
+						goBack();
+					}}
+				>
+					<Text>Cadastrar</Text>
+				</Button>
+			</Content>
+		</Container>
+	);
 }
