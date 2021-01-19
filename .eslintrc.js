239361c71b00dcd4f9a522778a15f4b7e3994a8b
Nicolas <nicolasterramaia@gmail.com>
2021-01-19 20:49:37 -0300

    chore: eslint and prettier configs changed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier/recommended', '@react-native-community'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off',
+  },
 };
