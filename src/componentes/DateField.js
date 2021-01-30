07c5f9af2b7ae9f077a3aa1788af6cccf85a361e
Nicolas <nicolasterramaia@gmail.com>
2021-01-30 17:44:10 -0300

    refact: convert DateField into functional compoent

--- src/componentes/DateField.js
+++ src/componentes/DateField.js
@@ -1,36 +1,34 @@
 import { DatePicker, Input, Item } from 'native-base';
 import React from 'react';
 
-export default class DateField extends React.Component {
-  render() {
-    return (
-      <Item
-        style={{
-          flex: 1,
-          minHeight: this.props.tamanho,
-        }}
-      >
-        <DatePicker
-          defaultDate={new Date()}
-          maximumDate={new Date()}
-          locale='pt'
-          modalTransparent={false}
-          animationType='fade'
-          androidMode='spinner'
-          placeHolderText={this.props.placeholder}
-          textStyle={{ color: this.props.cor }}
-          placeHolderTextStyle={{ color: this.props.corP }}
-          onDateChange={this.props.onDateChange}
-        />
-        <Input
-          placeholder={
-            this.props.data
-              ? '(atual: ' + new Date(this.props.data).toDateString() + ')'
-              : ''
-          }
-          disabled
-        ></Input>
-      </Item>
-    );
-  }
-}
+export default DateField = (props) => {
+  return (
+    <Item
+      style={{
+        flex: 1,
+        minHeight: props.tamanho,
+      }}
+    >
+      <DatePicker
+        defaultDate={new Date()}
+        maximumDate={new Date()}
+        locale='pt'
+        modalTransparent={false}
+        animationType='fade'
+        androidMode='spinner'
+        placeHolderText={props.placeholder}
+        textStyle={{ color: props.cor }}
+        placeHolderTextStyle={{ color: props.corP }}
+        onDateChange={props.onDateChange}
+      />
+      <Input
+        placeholder={
+          props.data
+            ? '(atual: ' + new Date(props.data).toDateString() + ')'
+            : ''
+        }
+        disabled
+      ></Input>
+    </Item>
+  );
+};
