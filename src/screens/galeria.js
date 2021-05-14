71e2bcb43599427dad00e33543fdf4417c78e0db
Nicolas <nicolasterramaia@gmail.com>
2021-05-14 09:52:12 -0300

    refact: change galeria to functional component

--- src/screens/galeria.js
+++ src/screens/galeria.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Dimensions,
   Image,
@@ -16,77 +16,22 @@ import { Banco } from '../instancias/conexao.js';
 
 var contador = 0;
 
-var galeria = {};
+export default function Galeria({ navigation }) {
+  const [fabAtivo, setFabAtivo] = useState(false);
+  const [apagar, setApagar] = useState(false);
+  const [fotos, setFotos] = useState([]);
 
-export default class Galeria extends React.Component {
-  constructor(props) {
-    super(props);
+  const { navigate, openDrawer } = navigation;
 
-    let tmp = [
-      {
-        id: '1',
-        local:
-          'http://portal.ufrrj.br/wp-content/uploads/2015/08/Foto-12-Veiculos-novos-para-UFRRJ-1150x300_c.jpg',
-      },
-      {
-        id: '3',
-        local:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3fd4VEsbvlV9zID-0KJBialiic49l41ojh7_QtEiFsYYIVyB6',
-      },
-      {
-        id: '4',
-        local:
-          'http://3.bp.blogspot.com/_qS8YczRuwno/TQPpYLklT3I/AAAAAAAAABc/_qTttWOuShU/s1600/Arquitetura_UFRRJ.jpg',
-      },
-      {
-        id: '5',
-        local:
-          'https://3.kekantoimg.com/_KCltK0xNROak71P6zCLwYeHGO4=/520x205/s3.amazonaws.com/kekanto_pics/pics/63/648063.jpg',
-      },
-      {
-        id: '6',
-        local:
-          'http://r1.ufrrj.br/petsi/sudestepetrj2014/wp-content/uploads/2012/08/60872_113846268781206_333392715_n-e1389547390547.jpg',
-      },
-      {
-        id: '7',
-        local:
-          'https://i0.wp.com/www.seropedicaonline.com/wp-content/uploads/2015/03/P4.jpg',
-      },
-    ];
-
-    this.state = {
-      fabAtivo: false,
-      apagar: false,
-      fotos: tmp,
-    };
-  }
-
-  static navigationOptions = {
+  const navigationOptions = {
     tabBarIcon: ({ tintColor }) => (
       <Ionicon name='md-photos' color={tintColor} style={{ fontSize: 30 }} />
     ),
   };
 
-  componentDidMount = () => {
-    Banco.local
-      .get('galeria')
-      .then((doc) => {
-        galeria = doc;
-        this.setState({ fotos: galeria.fotos });
-        // console.warn(doc);
-      })
-      .catch((erro) => {
-        // console.warn(erro);
-        galeria = {
-          _id: 'galeria',
-        };
-      });
-  };
-
-  renderizaFotos = () => {
+  const renderizaFotos = () => {
     var tmp = (Dimensions.get('window').width - 40) / 3;
-    return this.state.fotos.map((foto) => (
+    return fotos.map((foto) => (
       <Lightbox
         style={{ margin: 5, backgroundColor: '#aaa' }}
         key={++contador}
@@ -108,7 +53,7 @@ export default class Galeria extends React.Component {
         }}
       >
         <View>
-          {this.state.apagar == true && (
+          {apagar == true && (
             <Button
               danger
               style={{
@@ -134,18 +79,7 @@ export default class Galeria extends React.Component {
                     },
                     {
                       text: 'Sim',
-                      onPress: async () => {
-                        var tmp = this.state.fotos;
-                        for (var i = 0; i < tmp.length; i++) {
-                          if (tmp[i] == foto) tmp.splice(i, 1);
-                        }
-                        this.setState({ fotos: tmp });
-                        galeria.fotos = tmp;
-                        Banco.local.put(galeria, function (erro, doc) {
-                          if (erro) return;
-                          galeria._rev = doc._rev;
-                        });
-                      },
+                      onPress: async () => {},
                     },
                   ],
                   { cancelable: false }
@@ -170,90 +104,79 @@ export default class Galeria extends React.Component {
     ));
   };
 
-  render() {
-    const { navigate, openDrawer } = this.props.navigation;
-    return (
-      <Container>
-        <ImageBackground
-          source={require('../assets/myfarm_bg_grass.jpg')}
-          blurRadius={10}
+  return (
+    <Container>
+      <ImageBackground
+        source={require('../assets/myfarm_bg_grass.jpg')}
+        blurRadius={10}
+        style={{
+          width: Dimensions.get('window').width,
+          height: Dimensions.get('window').height - 50,
+          flexDirection: 'column',
+        }}
+      >
+        {/* drawer */}
+        <TouchableOpacity
           style={{
-            width: Dimensions.get('window').width,
-            height: Dimensions.get('window').height - 50,
+            position: 'absolute',
+            marginTop: 35,
+            marginLeft: 15,
+          }}
+          onPress={() => openDrawer()}
+        >
+          <Ionicon style={{ color: 'white', fontSize: 45 }} name='md-menu' />
+        </TouchableOpacity>
+
+        {/* view com o resto da pagina */}
+        <View
+          style={{
+            flex: 1,
+            marginTop: 100,
+            justifyContent: 'center',
+            alignItems: 'center',
             flexDirection: 'column',
           }}
         >
-          {/* drawer */}
-          <TouchableOpacity
-            style={{
-              position: 'absolute',
-              marginTop: 35,
-              marginLeft: 15,
-            }}
-            onPress={() => openDrawer()}
-          >
-            <Ionicon style={{ color: 'white', fontSize: 45 }} name='md-menu' />
-          </TouchableOpacity>
-
-          {/* view com o resto da pagina */}
-          <View
-            style={{
-              flex: 1,
-              marginTop: 100,
-              justifyContent: 'center',
-              alignItems: 'center',
-              flexDirection: 'column',
-            }}
-          >
-            {/* lista de fotos */}
-            <ScrollView>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  flexWrap: 'wrap',
-                }}
-              >
-                {this.renderizaFotos()}
-              </View>
-            </ScrollView>
-          </View>
-
-          {/* botao de ações flutuante */}
-          <View style={{ flex: 1 }}>
-            <Fab
-              active={this.state.active}
-              direction='left'
-              position='bottomRight'
-              style={{ backgroundColor: '#629c44' }}
-              onPress={() =>
-                this.setState({
-                  active: !this.state.active,
-                })
-              }
+          {/* lista de fotos */}
+          <ScrollView>
+            <View
+              style={{
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+              }}
             >
-              <Icon name='md-camera' />
+              {renderizaFotos()}
+            </View>
+          </ScrollView>
+        </View>
 
-              <Button
-                style={{ backgroundColor: '#44869c' }}
-                onPress={() => navigate('Camera')}
-              >
-                <Icon name='md-aperture' />
-              </Button>
+        {/* botao de ações flutuante */}
+        <View style={{ flex: 1 }}>
+          <Fab
+            active={fabAtivo}
+            direction='left'
+            position='bottomRight'
+            style={{ backgroundColor: '#629c44' }}
+            onPress={() => setFabAtivo((prevState) => !prevState)}
+          >
+            <Icon name='md-camera' />
 
-              <Button
-                style={{ backgroundColor: '#CD6B69' }}
-                onPress={() =>
-                  this.setState({
-                    apagar: !this.state.apagar,
-                  })
-                }
-              >
-                <Icon name='md-trash' />
-              </Button>
-            </Fab>
-          </View>
-        </ImageBackground>
-      </Container>
-    );
-  }
+            <Button
+              style={{ backgroundColor: '#44869c' }}
+              onPress={() => navigate('Camera')}
+            >
+              <Icon name='md-aperture' />
+            </Button>
+
+            <Button
+              style={{ backgroundColor: '#CD6B69' }}
+              onPress={() => setApagar((prevState) => !prevState)}
+            >
+              <Icon name='md-trash' />
+            </Button>
+          </Fab>
+        </View>
+      </ImageBackground>
+    </Container>
+  );
 }
